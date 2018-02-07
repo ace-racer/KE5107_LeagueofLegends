38ba0d5676872f1d0399c7e810d415bb09df8761
Anurag Chatterjee <anuragchatterjee92@gmail.com>
2018-02-07 15:16:09 +0800

    Add code to show legend with the symbols used in plot and clean code

--- discovery.R
+++ discovery.R
@@ -40,8 +40,19 @@ for (matchNumber in 1:nrow(matches_data)) {
   matches_data$ratioBlueMoreGoldThanRed[matchNumber] <- timesBlueWithMoreGoldThanRed/matchLength
 }
 
+timesBlueTeamWonForRatio = list()
+timesBlueTeamLoseForRatio = list()
+
 for (i in 1:10){
   ratioOfMatchBlueLedInGold <- 0.1 * i
   timesBlueTeamWonForRatio[i] <- length(which(matches_data$bResult[matches_data$ratioBlueMoreGoldThanRed > (ratioOfMatchBlueLedInGold - 0.1) & matches_data$ratioBlueMoreGoldThanRed <= ratioOfMatchBlueLedInGold] == 1))
   timesBlueTeamLoseForRatio[i] <- length(which(matches_data$bResult[matches_data$ratioBlueMoreGoldThanRed > (ratioOfMatchBlueLedInGold - 0.1) & matches_data$ratioBlueMoreGoldThanRed <= ratioOfMatchBlueLedInGold] == 0))
 }
+
+# scatter plot of the 2 different values
+xValues <- seq(10, 100, by = 10)
+plot(xValues, timesBlueTeamWonForRatio, type="p", main="Advantage in gold possession with winning", xlab="Percent of total match Blue team had more gold than Red team", ylab="Number of matches won", col="blue")
+par(new = TRUE)
+plot(xValues, timesBlueTeamLoseForRatio, type="p", col="red", axes = FALSE, xlab="", ylab="")
+axis(1, at = xValues)
+legend("top", legend = c("Matches won by Blue team", "Matches won by Red team"), col = c("blue", "red"), cex = 0.8, pch=1)
