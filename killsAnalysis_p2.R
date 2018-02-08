a6119c12fe81133080d3826d745419f02cd8839e
Anurag Chatterjee <anuragchatterjee92@gmail.com>
2018-02-08 23:31:37 +0800

    Create correlation plots between kills and match result

---
+++ killsAnalysis_p2.R
@@ -0,0 +1,30 @@
+library(corrplot)
+
+setwd("H:\\KE 5107 Projects\\code\\KE5107_LeagueofLegends")
+matches_data = read.csv("processed/matches.csv")
+game_elements <- c("Towers", "Inhibs", "Dragons", "Barons", "Heralds") 
+
+# Get the column name for the red team
+getRedColumnName <- function(p){
+  paste("r", p, "Num", sep="")
+}
+
+# Get the column name for the blue team
+getBlueColumnName <- function(p){
+  paste("b", p, "Num", sep="")
+}
+
+# Get the number of Kills columns for blue and red teams
+blue_num_kills_columns <- lapply(game_elements, getBlueColumnName)
+red_num_kills_columns <- lapply(game_elements, getRedColumnName)
+
+# Combine the Kills columns
+all_num_kills_columns <- c(blue_num_kills_columns, red_num_kills_columns)
+
+all_num_kills_columns_with_result <- c(all_num_kills_columns, "bResult")
+
+# head(matches_data[,unlist(all_num_kills_columns_with_result)])
+
+# Get correlation between the number of kills and the result of the match
+killscor <- cor(matches_data[,unlist(all_num_kills_columns_with_result)])
+corrplot(killscor, method = "circle")
